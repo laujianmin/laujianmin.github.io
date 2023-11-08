123beacd350a9c3863db4d494ff04f121704f783
laujianmin <kinnisoy@gmail.com>
2023-11-08 17:02:43 +0800

    commit

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -14,50 +14,51 @@ os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
 
+
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['citedby']}",
+  # "message": f"{author['citedby']}",
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
 
-shieldio_data_mtl = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['publications']['zigGlzAAAAAJ:bEWYMUwI8FkC']['num_citations']}",
-}
-with open(f'results/gs_data_shieldsio_mtl.json', 'w') as outfile:
-    json.dump(shieldio_data_mtl, outfile, ensure_ascii=False)
+# shieldio_data_mtl = {
+#   "schemaVersion": 1,
+#   "label": "citations",
+#   "message": f"{author['publications']['zigGlzAAAAAJ:u5HHmVD_uO8C']['num_citations']}",
+# }
+# with open(f'results/gs_data_shieldsio_mtl.json', 'w') as outfile:
+#     json.dump(shieldio_data_mtl, outfile, ensure_ascii=False)
     
-shieldio_data_mnemonics = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['publications']['zigGlzAAAAAJ:k_IJM867U9cC']['num_citations']}",
-}
-with open(f'results/gs_data_shieldsio_mnemonics.json', 'w') as outfile:
-    json.dump(shieldio_data_mnemonics, outfile, ensure_ascii=False)
+# shieldio_data_mnemonics = {
+#   "schemaVersion": 1,
+#   "label": "citations",
+#   "message": f"{author['publications']['zigGlzAAAAAJ:k_IJM867U9cC']['num_citations']}",
+# }
+# with open(f'results/gs_data_shieldsio_mnemonics.json', 'w') as outfile:
+#     json.dump(shieldio_data_mnemonics, outfile, ensure_ascii=False)
     
-shieldio_data_aanets = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['publications']['zigGlzAAAAAJ:u_35RYKgDlwC']['num_citations']}",
-}
-with open(f'results/gs_data_shieldsio_aanets.json', 'w') as outfile:
-    json.dump(shieldio_data_aanets, outfile, ensure_ascii=False)
+# shieldio_data_aanets = {
+#   "schemaVersion": 1,
+#   "label": "citations",
+#   "message": f"{author['publications']['zigGlzAAAAAJ:u_35RYKgDlwC']['num_citations']}",
+# }
+# with open(f'results/gs_data_shieldsio_aanets.json', 'w') as outfile:
+#     json.dump(shieldio_data_aanets, outfile, ensure_ascii=False)
     
-shieldio_data_e3bm = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['publications']['zigGlzAAAAAJ:vV6vV6tmYwMC']['num_citations']}",
-}
-with open(f'results/gs_data_shieldsio_e3bm.json', 'w') as outfile:
-    json.dump(shieldio_data_e3bm, outfile, ensure_ascii=False)
+# shieldio_data_e3bm = {
+#   "schemaVersion": 1,
+#   "label": "citations",
+#   "message": f"{author['publications']['zigGlzAAAAAJ:vV6vV6tmYwMC']['num_citations']}",
+# }
+# with open(f'results/gs_data_shieldsio_e3bm.json', 'w') as outfile:
+#     json.dump(shieldio_data_e3bm, outfile, ensure_ascii=False)
    
-shieldio_data_lst = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['publications']['zigGlzAAAAAJ:TFP_iSt0sucC']['num_citations']}",
-}
-with open(f'results/gs_data_shieldsio_lst.json', 'w') as outfile:
-    json.dump(shieldio_data_lst, outfile, ensure_ascii=False)
+# shieldio_data_lst = {
+#   "schemaVersion": 1,
+#   "label": "citations",
+#   "message": f"{author['publications']['zigGlzAAAAAJ:TFP_iSt0sucC']['num_citations']}",
+# }
+# with open(f'results/gs_data_shieldsio_lst.json', 'w') as outfile:
+#     json.dump(shieldio_data_lst, outfile, ensure_ascii=False)
